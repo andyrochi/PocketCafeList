c3fea1fb82739c6468e489a46992371557cf5a29
andyrochi <andy.rochi@gmail.com>
2022-06-22 17:58:46 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const command = require('./controllers/commandHandler');
+
+// Obtain token and secret from .env if not production
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // handle text message with commandHandler
+  const echo = command.commandHandler(event.source, event.message);
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
